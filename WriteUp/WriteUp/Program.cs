68525beb6be32ea03af87dad03bb05612c25c799
Pracheeti S <46408357+PeachyP33@users.noreply.github.com>
2019-10-29 22:40:18 -0400

    pptx slide proj2

---
+++ WriteUp/WriteUp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num;
+            num = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Counting up with a loop...");
+            writeUpRecursively(num);
+            Console.WriteLine("Counting up with recursively...");
+            writeDownLoop(num);
+
+
+            Console.ReadLine();
+        }
+
+        static void writeUpRecursively(int n)
+        {
+            if (n > 1)  // stopping/continuing conditon            
+            {                writeUpRecursively(n - 1);  // modify argument            
+            }
+            Console.WriteLine("{0}", n);
+        }
+
+        static void writeDownLoop(int n) {
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine("{0}", i);
+            }
+        }
+    }
+}
