f28d658591e8f1b84893f567ebef554644a7b163
Hudak123 <57154322+Hudak123@users.noreply.github.com>
2019-12-03 08:34:45 -0500

    Add files via upload

---
+++ ListExercise3AddColors/ListExercise3AddColors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExercise3AddColors
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            List<string> colors = new List<string>() {"blue","pink", "purple"};
+            List<string> newColors = new List<string>() {"violet", "navy"}; //blue, pink, purple, violet, navy
+            colors.AddRange(newColors);
+            Console.ReadLine();
+
+            Console.WriteLine("The colors in the first list are: ");
+            Console.WriteLine("blue  pink  purple");
+            Console.ReadLine();
+
+            Console.WriteLine("The colors in the second list are: ");
+            Console.WriteLine("navy  violet");
+            Console.ReadLine();
+
+            Console.WriteLine("The colors in the list after using AddRange: ");
+            Console.WriteLine("blue  pink  purple  navy  violet");
+
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
