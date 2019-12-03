2e1d30e219979563c104784780027a15a007f4eb
Pracheeti S <46408357+PeachyP33@users.noreply.github.com>
2019-12-03 08:19:18 -0500

    addColors

---
+++ AddColors/AddColors/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddColors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> colors1 = new List<string>() { "blue", "pink", "purple" };
+            List<string> colors2 = new List<string>() { "navy", "violet" };
+
+            Console.WriteLine("The colors in your first list are:");
+            for (int i = 0; i < colors1.Count(); i++)
+            {
+                Console.Write(colors1[i] + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("The colors in your second list are:");
+            for (int i = 0; i < colors2.Count(); i++)
+            {
+                Console.Write(colors2[i] + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            colors1.AddRange(colors2);
+            Console.WriteLine("The colors in your combined list are:");
+            for (int i = 0; i < colors1.Count(); i++)
+            {
+                Console.Write(colors1[i] + ", ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
