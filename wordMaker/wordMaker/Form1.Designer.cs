4bdd32d861a502b748c6c892a57d509db74c49f7
Pracheeti S <46408357+PeachyP33@users.noreply.github.com>
2019-12-03 08:15:59 -0500

    wordMaker

---
+++ wordMaker/wordMaker/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace wordMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lstWords = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(77, 54);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(165, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(120, 116);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lstWords
+            // 
+            this.lstWords.FormattingEnabled = true;
+            this.lstWords.Location = new System.Drawing.Point(98, 168);
+            this.lstWords.Name = "lstWords";
+            this.lstWords.Size = new System.Drawing.Size(120, 95);
+            this.lstWords.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(324, 320);
+            this.Controls.Add(this.lstWords);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.ListBox lstWords;
+    }
+}
+
---
+++ wordMaker/wordMaker/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wordMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            string input = txtInput.Text;
+            List<string> phrase = new List<string>(input.Split());
+            
+            for (int i = 0; i < phrase.Count(); i++) {
+                string hi = phrase[i];
+                lstWords.Items.Add(hi);
+            }
+            
+
+        }
+    }
+}
