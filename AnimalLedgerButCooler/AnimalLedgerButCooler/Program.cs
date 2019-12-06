8ef5c73a97ff5a8ed5ae0abd862b8953eabe8e12
Hudak123 <57154322+Hudak123@users.noreply.github.com>
2019-12-06 08:41:07 -0500

    Add files via upload

---
+++ AnimalLedgerButCooler/AnimalLedgerButCooler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalLedgerButCooler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> animals = new List<string>();
+            animals.Add("Dog1 is at the vet");
+            animals.Add("Dog2 is not at the vet");
+            animals.Add("Dog3 is at the vet");
+            animals.Add("Dog4 is at the vet");
+            animals.Add("Dog5 is not at the vet");
+            animals.Add("Dog6 is at the vet");
+            animals.ForEach(Console.WriteLine);
+            Console.ReadLine();
+        }
+    }
+}
