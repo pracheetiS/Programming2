475c69380da6de9a6bb23673ba53d9530885c27f
Aiden-Hall <58669390+Aiden-Hall@users.noreply.github.com>
2019-12-12 08:13:04 -0500

    Add files via upload

---
+++ EnvironmentDemoFile (1)/EnvironmentDemo/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace EnvironmentDemo
+{
+    partial class FrmEnvironment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abootToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LB = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(67, 28);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(704, 52);
+            this.btnDisplay.TabIndex = 0;
+            this.btnDisplay.Text = "Display Environment Data";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.abootToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(889, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // abootToolStripMenuItem
+            // 
+            this.abootToolStripMenuItem.Name = "abootToolStripMenuItem";
+            this.abootToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.abootToolStripMenuItem.Text = "About";
+            this.abootToolStripMenuItem.Click += new System.EventHandler(this.abootToolStripMenuItem_Click);
+            // 
+            // LB
+            // 
+            this.LB.FormattingEnabled = true;
+            this.LB.Location = new System.Drawing.Point(31, 86);
+            this.LB.Name = "LB";
+            this.LB.Size = new System.Drawing.Size(760, 251);
+            this.LB.TabIndex = 3;
+            // 
+            // FrmEnvironment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 354);
+            this.Controls.Add(this.LB);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmEnvironment";
+            this.Text = "Environment Demo";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abootToolStripMenuItem;
+        private System.Windows.Forms.ListBox LB;
+    }
+}
+
---
+++ EnvironmentDemoFile (1)/EnvironmentDemo/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EnvironmentDemo
+{
+    public partial class FrmEnvironment : Form
+    {
+        public FrmEnvironment()
+        {
+            InitializeComponent();
+        }
+        string[] drives;
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            OperatingSystem os = Environment.OSVersion;
+            PlatformID osId = os.Platform;
+            drives = Environment.GetLogicalDrives();
+            string drive = "";
+            foreach (string driv in drives)
+            {
+               
+                drive += driv + " ";
+            }
+            LB.Items.Add("Operating Sys   " + os);
+            LB.Items.Add("Operating Sys ID  " + osId);
+            LB.Items.Add("CLR Version    " + Environment.Version);
+            LB.Items.Add("Machine name    " + Environment.MachineName);
+            LB.Items.Add("");
+            LB.Items.Add("User ID + \t\t" + Environment.UserName);
+            LB.Items.Add("Current Directory\t" + Environment.CurrentDirectory);
+            LB.Items.Add("Drives Present\t" + drive);
+
+
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void abootToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Demonstration Program for reading some of the available " + "\n\r Fall 2019 for NC CP2, T35");
+        }
+    }
+}
