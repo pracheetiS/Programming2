31d95dc0adb39e419c95bd4f75feb5bddf756780
Aiden-Hall <58669390+Aiden-Hall@users.noreply.github.com>
2019-12-13 08:20:40 -0500

    Add files via upload

---
+++ pptxpart1(1)/pptxpart1(1)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pptxpart1_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string fileName = @"\\testRead.txt";
+            //using (StreamWriter fileStr = File.SpecialFolder.Personal.CreateText(fileName))
+            //{
+            //    fileStr.WriteLine("Hello and Welcome");
+            //    Console.WriteLine(" It is the first content");
+            //    Console.WriteLine(" of the text file mytest.txt");
+            //    Console.WriteLine(" A file created with the content name mytest\n\n");
+            //    Console.ReadLine();
+            //}
+            string fileName = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\testRead.txt";  // this resolves to My Documents
+            string readText = File.ReadAllText(fileName);
+            Console.WriteLine(readText);
+            string textToAdd = "Hello World" + "\n" + readText;
+          
+                
+            File.WriteAllText( fileName, textToAdd);
+            File.AppendAllText(fileName, textToAdd);
+            Console.ReadLine();
+        }
+    }
+}
