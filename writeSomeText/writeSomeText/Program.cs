00d832fcc5238629ec5b62809b0be6e2169d39d3
Pracheeti S <46408357+PeachyP33@users.noreply.github.com>
2019-12-13 08:23:24 -0500

    pptxPart4

---
+++ writeSomeText/writeSomeText/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace writeSomeText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\SomeText.txt";
+            FileStream outFile = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(outFile);
+            Console.WriteLine("Enter some text to be written to a file.");
+            string response = Console.ReadLine();
+            writer.WriteLine(response);
+            // The following must be done in order
+            writer.Close();
+            outFile.Close();
+
+        }
+    }
+}
