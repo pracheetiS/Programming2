e3b032c2cd059b561cfc7d1639d14b2beb450c67
Pracheeti S <46408357+PeachyP33@users.noreply.github.com>
2020-01-08 08:03:03 -0500

    Add files via upload

---
+++ CountIntegers/CountIntegers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l1 = new List<int>() { 1,4,5,7,7,8,7,7,4,6,1,1,1 };
+
+            var g = l1.GroupBy(i => i);
+
+            foreach (int n in l1)
+            {
+                Console.Write(n.ToString() + " ");
+            }
+            Console.WriteLine("\n");
+
+            foreach (var grp in g)
+            {
+                Console.WriteLine("{0} occurred {1} time(s)", grp.Key, grp.Count());
+            }
+            Console.ReadLine();
+        }
+    }
+}
