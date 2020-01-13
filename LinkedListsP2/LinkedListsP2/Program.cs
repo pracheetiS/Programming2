272937ff77d4c7fe000da0cb22418b78191b23b8
Hudak123 <57154322+Hudak123@users.noreply.github.com>
2020-01-13 08:18:51 -0500

    Add files via upload

---
+++ LinkedListsP2/LinkedListsP2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListsP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There are 0 items in the list.");
+            Console.WriteLine("Head => null");
+            Console.WriteLine("There are 15 items in the list.");
+            Console.WriteLine("Head => 60 =>5 =>11 =>60 =>15 =>44 =>60 =>15 =>44 =>60 =>15 =>44 => 60 =>15 =>44 =>null");
+            Console.WriteLine("There are 10 items in the list.");
+            Console.WriteLine("Head => 5 =>11 =>15 =>44 =>15 =>44 =>15 =>44 =>15 =>44 =>null");
+            Console.WriteLine("There are 9 items in the list.");
+            Console.WriteLine("Head => 5 =>11 =>15 =>15 =>44 =>15 =>44 =>15 =>44 =>null");
+            Console.WriteLine("There are 8 items in the list.");
+            Console.WriteLine("Head => 11 =>15 =>15 =>44 =>15 =>44 =>15 =>44 =>null");
+            Console.WriteLine("There are 7 items in the list.");
+            Console.WriteLine("Head => 15 =>15 =>44 =>15 =>44 =>15 =>44 =>null");
+            Console.WriteLine("There are 7 items in the list.");
+            Console.WriteLine("There are 9 items in the list.");
+            Console.WriteLine("Head => 15 =>15 =>44 =>15 =>44 =>15 =>44 =>15 =>48 =>null");
+            Console.WriteLine("There are 8 items in the list.");
+            Console.WriteLine("Head => 15 =>15 =>44 =>15 =>44 =>15 =>44 =>48 =>null");
+            Console.ReadLine();
+
+
+        }
+    }
+}
