f5b58b122d919ee080c12c4059cd1bc9c93b3cd1
kawaakari03 <57158453+kawaakari03@users.noreply.github.com>
2020-01-17 07:45:50 -0500

    Add files via upload

---
+++ UserInputExC/UserInputExC/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInputExC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(Console.ReadLine() == "cat")
+            {
+                Console.WriteLine("meow");
+            }
+            else if(Console.ReadLine() == "dog")
+            {
+                Console.WriteLine("woof");
+            }
+            else if (Console.ReadLine() == "bird")
+            {
+                Console.WriteLine("tweet");
+            }
+            else
+            {
+                Console.WriteLine("Don't you like animals?");
+            }
+            Console.ReadLine();
+        }
+    }
+}
