1848c25d32fb9e1947c440056253b873d0acbb16
Pracheeti S <46408357+PeachyP33@users.noreply.github.com>
2020-01-17 08:46:02 -0500

    mastermind

---
+++ MasterMind/MasterMind/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace MasterMind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.ComboBox();
+            this.num2 = new System.Windows.Forms.ComboBox();
+            this.num3 = new System.Windows.Forms.ComboBox();
+            this.num4 = new System.Windows.Forms.ComboBox();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.FormattingEnabled = true;
+            this.num1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.num1.Location = new System.Drawing.Point(43, 173);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(57, 21);
+            this.num1.TabIndex = 0;
+            // 
+            // num2
+            // 
+            this.num2.FormattingEnabled = true;
+            this.num2.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.num2.Location = new System.Drawing.Point(119, 173);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(57, 21);
+            this.num2.TabIndex = 1;
+            // 
+            // num3
+            // 
+            this.num3.FormattingEnabled = true;
+            this.num3.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.num3.Location = new System.Drawing.Point(201, 173);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(57, 21);
+            this.num3.TabIndex = 2;
+            // 
+            // num4
+            // 
+            this.num4.FormattingEnabled = true;
+            this.num4.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.num4.Location = new System.Drawing.Point(281, 173);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(57, 21);
+            this.num4.TabIndex = 3;
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(371, 13);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(205, 420);
+            this.lstBox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(138, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 43);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 41);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Guess the PIN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(92, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "clue 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(213, 388);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 32);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "clue 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MasterMind.Properties.Resources._635988;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(588, 445);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox num1;
+        private System.Windows.Forms.ComboBox num2;
+        private System.Windows.Forms.ComboBox num3;
+        private System.Windows.Forms.ComboBox num4;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ MasterMind/MasterMind/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterMind
+{
+    public partial class Form1 : Form
+    {
+        int answer = 1234;
+        int guess = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string numIntered = num1.Text + num2.Text + num3.Text + num4.Text;
+            if (num1.Text == "1" && num2.Text == "2" && num3.Text == "3" && num4.Text == "4")
+            {
+                guess++;
+                lstBox.Items.Add("Guesses: " + guess);
+                lstBox.Items.Add("PIN: " + numIntered);
+                lstBox.Items.Add("CORRECT");
+                lstBox.Items.Add("------------------------");
+            }
+            else {
+                guess++;
+                lstBox.Items.Add("Guesses: " + guess);
+                lstBox.Items.Add("PIN: " + numIntered);
+                lstBox.Items.Add("INCORRECT");
+                lstBox.Items.Add("------------------------");
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The number 3 is in the second position from the right");
+            lstBox.Items.Add("Opened Clue 1");
+            lstBox.Items.Add("------------------------");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The number 1 is used somewhere");
+            lstBox.Items.Add("Opened Clue 2");
+            lstBox.Items.Add("------------------------");
+
+        }
+    }
+}
